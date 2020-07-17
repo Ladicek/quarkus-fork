084cac1a56ef65bbed19b4796508fff1da39c7b8
Ladislav Thon <lthon@redhat.com>
2020-07-17 13:24:02 +0200

    initial experiment with implementing a declarative CDI Lite extension API

--- independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/BeanProcessor.java
+++ independent-projects/arc/processor/src/main/java/io/quarkus/arc/processor/BeanProcessor.java
@@ -6,6 +6,7 @@ import io.quarkus.arc.processor.BuildExtension.BuildContext;
 import io.quarkus.arc.processor.BuildExtension.Key;
 import io.quarkus.arc.processor.ResourceOutput.Resource;
 import io.quarkus.arc.processor.ResourceOutput.Resource.SpecialType;
+import io.quarkus.arc.processor.cdi.lite.ext.CdiLiteExtProcessor;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -73,6 +74,8 @@ public class BeanProcessor {
     protected final Predicate<DotName> injectionPointAnnotationsPredicate;
 
     private BeanProcessor(Builder builder) {
+        new CdiLiteExtProcessor(builder.index, builder).run();
+
         this.reflectionRegistration = builder.reflectionRegistration;
         this.applicationClassPredicate = builder.applicationClassPredicate;
         this.name = builder.name;
