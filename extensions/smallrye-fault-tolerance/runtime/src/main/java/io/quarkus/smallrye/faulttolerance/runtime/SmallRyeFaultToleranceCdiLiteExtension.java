f01ef3fff90ddaba0fa75b27ac892e96c5460645
Ladislav Thon <lthon@redhat.com>
2020-11-09 14:44:23 +0100

    second attempt at a push-based extension API
    The main difference compared to the first attempt is that
    annotation transformation callbacks are either declared
    or registered programmatically.
    
    The `@Enhancement` methods are required to declare exactly
    one parameter of a type from the following set:
    
    - `ClassConfig`, `MethodConfig` or `FieldConfig`: the method
       itself is an annotation transformation callback;
    - `AppArchiveConfg`: the method is executed to register
      annotation transformation callbacks programmatically.
    
    The `@ExactType` and `@SubtypesOf` annotations can now be placed
    on methods and are used in the declarative approach to constraint
    for which types (and with which annotations) is the method called.
    
    The `@Enhancement` methods can still declare parameters
    of "utility" types, if that proves useful.

--- extensions/smallrye-fault-tolerance/runtime/src/main/java/io/quarkus/smallrye/faulttolerance/runtime/SmallRyeFaultToleranceCdiLiteExtension.java
+++ extensions/smallrye-fault-tolerance/runtime/src/main/java/io/quarkus/smallrye/faulttolerance/runtime/SmallRyeFaultToleranceCdiLiteExtension.java
@@ -3,12 +3,7 @@ package io.quarkus.smallrye.faulttolerance.runtime;
 import java.util.OptionalInt;
 
 import javax.annotation.Priority;
-import javax.interceptor.Interceptor;
-import javax.interceptor.InterceptorBinding;
 
-import cdi.lite.extension.BuildCompatibleExtension;
-import cdi.lite.extension.phases.enhancement.ExactType;
-import io.smallrye.faulttolerance.FaultToleranceExtension;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.faulttolerance.Asynchronous;
@@ -18,57 +13,60 @@ import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Retry;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 
+import cdi.lite.extension.BuildCompatibleExtension;
 import cdi.lite.extension.phases.Enhancement;
 import cdi.lite.extension.phases.enhancement.Annotations;
-import cdi.lite.extension.phases.enhancement.AppArchiveConfig;
-import cdi.lite.extension.phases.enhancement.ClassEntrypoint;
+import cdi.lite.extension.phases.enhancement.ClassConfig;
+import cdi.lite.extension.phases.enhancement.ExactType;
 import io.smallrye.faulttolerance.FaultToleranceBinding;
 import io.smallrye.faulttolerance.FaultToleranceInterceptor;
 
 public class SmallRyeFaultToleranceCdiLiteExtension implements BuildCompatibleExtension {
     @Enhancement
-    public void process(
-            @ExactType(type = Asynchronous.class, annotatedWith = InterceptorBinding.class) ClassEntrypoint asynchronousClass,
-            @ExactType(type = Bulkhead.class, annotatedWith = InterceptorBinding.class) ClassEntrypoint bulkheadClass,
-            @ExactType(type = CircuitBreaker.class, annotatedWith = InterceptorBinding.class) ClassEntrypoint circuitBreakerClass,
-            @ExactType(type = Fallback.class, annotatedWith = InterceptorBinding.class) ClassEntrypoint fallbackClass,
-            @ExactType(type = Retry.class, annotatedWith = InterceptorBinding.class) ClassEntrypoint retryClass,
-            @ExactType(type = Timeout.class, annotatedWith = InterceptorBinding.class) ClassEntrypoint timeoutClass,
-            @ExactType(type = FaultToleranceInterceptor.class, annotatedWith = Interceptor.class) ClassEntrypoint ftInterceptorClass,
-            Annotations ann,
-            AppArchiveConfig app) {
+    @ExactType(type = Asynchronous.class)
+    @ExactType(type = Bulkhead.class)
+    @ExactType(type = CircuitBreaker.class)
+    @ExactType(type = Fallback.class)
+    @ExactType(type = Retry.class)
+    @ExactType(type = Timeout.class)
+    public void interceptorBinding(ClassConfig clazz) {
+        clazz.addAnnotation(FaultToleranceBinding.class);
+    }
 
-        asynchronousClass.addAnnotation(FaultToleranceBinding.class);
-        bulkheadClass.addAnnotation(FaultToleranceBinding.class);
-        circuitBreakerClass.addAnnotation(FaultToleranceBinding.class);
-        fallbackClass.addAnnotation(FaultToleranceBinding.class);
-        retryClass.addAnnotation(FaultToleranceBinding.class);
-        timeoutClass.addAnnotation(FaultToleranceBinding.class);
-
-        // alternative:
-//        app.classes()
-//                .exactly(Asynchronous.class)
-//                .exactly(Bulkhead.class)
-//                .exactly(CircuitBreaker.class)
-//                .exactly(Fallback.class)
-//                .exactly(Retry.class)
-//                .exactly(Timeout.class)
-//                .configure(it -> it.addAnnotation(new FaultToleranceBinding.Literal()));
-
-        // in the original Quarkus extension, this runs later (the build step consumes BeanArchiveIndexBuildItem)
-        // do we need to find a way how to express that in CDI Lite Extensions?
+    @Enhancement
+    @ExactType(type = FaultToleranceInterceptor.class)
+    public void interceptorPriority(ClassConfig clazz, Annotations ann) {
         Config config = ConfigProvider.getConfig();
         OptionalInt priority = config.getValue("mp.fault.tolerance.interceptor.priority", OptionalInt.class);
         if (priority.isPresent()) {
-            ftInterceptorClass.removeAnnotation(it -> it.name().equals(Priority.class.getName()));
-            ftInterceptorClass.addAnnotation(Priority.class, ann.attribute("value", priority.getAsInt()));
-            // alternative:
-//            app.classes()
-//                    .exactly(FaultToleranceInterceptor.class)
-//                    .configure(it -> {
-//                        it.removeAnnotation(ann -> ann.name().equals(Priority.class.getName()));
-//                        it.addAnnotation(new FaultToleranceExtension.PriorityLiteral(priority.getAsInt()));
-//                    });
+            clazz.removeAnnotation(it -> it.name().equals(Priority.class.getName()));
+            clazz.addAnnotation(Priority.class, ann.attribute("value", priority.getAsInt()));
         }
     }
+
+    // alternative
+/*
+    @Enhancement
+    public void process(AppArchiveConfig app) {
+        app.classes()
+                .exactly(Asynchronous.class)
+                .exactly(Bulkhead.class)
+                .exactly(CircuitBreaker.class)
+                .exactly(Fallback.class)
+                .exactly(Retry.class)
+                .exactly(Timeout.class)
+                .configure(it -> it.addAnnotation(new FaultToleranceBinding.Literal()));
+
+        Config config = ConfigProvider.getConfig();
+        OptionalInt priority = config.getValue("mp.fault.tolerance.interceptor.priority", OptionalInt.class);
+        if (priority.isPresent()) {
+            app.classes()
+                    .exactly(FaultToleranceInterceptor.class)
+                    .configure(it -> {
+                        it.removeAnnotation(ann -> ann.name().equals(Priority.class.getName()));
+                        it.addAnnotation(new FaultToleranceExtension.PriorityLiteral(priority.getAsInt()));
+                    });
+        }
+    }
+*/
 }
